f422c4367c8b262dfbaa895a880668aec2ecad4c
L12Raptor <raptorcreator12@gmail.com>
2023-05-04 17:06:11 +0200

    Tests selección de casilla inválida
    Creación de tests

--- tres_en_raya/src/main.rs
+++ tres_en_raya/src/main.rs
@@ -1,3 +1,46 @@
 fn main() {
     
 }
+
+#[test]
+fn test_select_row_less_than_zero_should_return_false()
+{
+    let row: i32 = -1;
+    let column: i32 = 0;
+
+    let result: bool = select_board_square(row, column);
+
+    assert!(!result);
+}
+
+fn test_select_column_less_than_zero_should_return_false()
+{
+    let row: i32 = 0;
+    let column: i32 = -1;
+
+    let result: bool = select_board_square(row, column);
+
+    assert!(!result);
+}
+
+#[test]
+fn test_select_row_greater_than_two_should_return_false()
+{
+    let row: i32 = 3;
+    let column: i32 = 0;
+
+    let result: bool = select_board_square(row, column);
+
+    assert!(!result);
+}
+
+#[test]
+fn test_select_column_greater_than_two_should_return_false()
+{
+    let row: i32 = 0;
+    let column: i32 = 3;
+
+    let result: bool = select_board_square(row, column);
+
+    assert!(!result);
+}
