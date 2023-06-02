507ff44be58a3cd742f082e37afe47bdcdb80cbe
L12Raptor <raptorcreator12@gmail.com>
2023-06-02 12:37:41 +0200

    Mejoras y detección de máquina virtual

--- informacion-so/src/main.rs
+++ informacion-so/src/main.rs
@@ -1,8 +1,37 @@
 fn main() {
-    let operating_system_info = os_info::get();
+    let osinfo = systeminfo::from_system_os();
 
-    println!("OS type: {}", operating_system_info.os_type());
-    println!("OS version: {}", operating_system_info.version());
-    println!("OS edition: {}", operating_system_info.edition().unwrap_or_default());
-    println!("OS architecture: {}", operating_system_info.architecture().unwrap_or_default());
+    println!("OS type: {}", osinfo.os);
+
+    println!("OS version: {}", osinfo.version);
+
+    println!("OS edition: {}", osinfo.edition);
+
+    println!("OS architecture: {}", osinfo.architecture);
+
+    println!();
+
+    let sysinfo = systeminfo::from_system_hardware();
+
+    let sys_manufacturer = sysinfo.system_manufacturer;
+
+    if !sys_manufacturer.is_empty() {
+        println!("System manufacturer: {}", sys_manufacturer);
+
+        let mut is_vm = false;
+
+        let vm_manufacturers = vec!["QEMU", "innotek", "VMware"];
+
+        for manufacturer in vm_manufacturers.iter() {
+            if sys_manufacturer.contains(manufacturer) {
+                is_vm = true;
+
+                break;
+            }
+        }
+
+        println!("Is Virtual Machine: {}", is_vm);
+    } else {
+        println!("Error getting system information, try running as administrator.");
+    }
 }
